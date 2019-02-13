fc69701c4ec4580cf7dcbeb083bb6c0dc22c8972
joshuacoyne <josh.j.coyne@gmail.com>
2019-02-13 17:45:05 -0500

    Updates

--- portfolio_site/src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import LandingPage from './landingPage';
-
-const IndexPage = () => {
-        return (
-            <LandingPage/>
-        )
-}
-export default IndexPage;
